bb2ba973c14e6b8ab3f77ef40074e21e111aad0f
anglicangeek <ego@anglicangeek.com>
2012-02-04 23:05:47 -0800

    added class lib for facts (unit tests)

---
+++ Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("PoliteCaptcha.Facts")]
+[assembly: AssemblyTitle("PoliteCaptcha.Facts")]
+[assembly: AssemblyDescription("Facts (unit tests) for PoliteCaptcha.")]
+[assembly: AssemblyCompany("OuterCurve Foundation")]
+[assembly: AssemblyCopyright("Copyright (C) 2012 OuterCurve Foundation")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: ComVisible(false)]
