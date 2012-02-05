e236409fd715d094e68d1b2bd189e0b299903883
anglicangeek <ego@anglicangeek.com>
2012-02-04 23:04:11 -0800

    created empty class lib project

---
+++ PoliteCaptcha/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("PoliteCaptcha")]
+[assembly: AssemblyTitle("PoliteCaptcha")]
+[assembly: AssemblyDescription("PoliteCaptcha is a spam prevention library for use with ASP.NET MVC that attempts polite spam prevention before rudely presenting the user with a CAPTCHA.")]
+[assembly: AssemblyCompany("OuterCurve Foundation")]
+[assembly: AssemblyCopyright("Copyright (C) 2012 OuterCurve Foundation")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: ComVisible(false)]
