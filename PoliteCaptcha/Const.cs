e319bf18118f8ab1beb8f151e9aa406c8e034a85
anglicangeek <ego@anglicangeek.com>
2012-02-06 11:05:05 -0800

    implemented the reCAPTCHA-based validator, the default CAPTCHA validator used by PoliteCaptcha

--- PoliteCaptcha/Const.cs
+++ PoliteCaptcha/Const.cs
@@ -7,5 +7,8 @@ namespace PoliteCaptcha
         public const string ModelStateKey = "PoliteCaptcha";
         public const string NoCaptchaChallengeField = "nocaptcha_challenge";
         public const string NoCaptchaResponseField = "nocaptcha_response";
+        public const string ReCaptchaChallengeField = "recaptcha_challenge_field";
+        public const string ReCaptchaLocalhostPrivateKey = "6Lde4cwSAAAAANih82mBN8lDAu_nFSKKwYIqGw4A";
+        public const string ReCaptchaResponseField = "recaptcha_response_field";
     }
 }
--- PoliteCaptcha/ReCaptchaValidator.cs
+++ PoliteCaptcha/ReCaptchaValidator.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Web;
+using Recaptcha;
 
 namespace PoliteCaptcha
 {
     public class ReCaptchaValidator : ICaptchaValidator
     {
+        readonly RecaptchaValidator recaptchaValidator;
+
+        public ReCaptchaValidator()
+        {
+            recaptchaValidator = new RecaptchaValidator
+            {
+                PrivateKey = Const.ReCaptchaLocalhostPrivateKey,
+            };
+        }
+        
         public bool Validate(HttpContextBase httpContext)
         {
-            throw new Exception();
+            if (httpContext == null)
+                throw new ArgumentNullException("httpContext");
+
+            var challenge = httpContext.Request.Form[Const.ReCaptchaChallengeField];
+            if (string.IsNullOrWhiteSpace(challenge))
+                return false;
+
+            var response = httpContext.Request.Form[Const.NoCaptchaResponseField];
+            if (string.IsNullOrWhiteSpace(response))
+                return false;
+
+            recaptchaValidator.Challenge = challenge;
+            recaptchaValidator.Response = response;
+            recaptchaValidator.RemoteIP = httpContext.Request.UserHostAddress;
+
+            return recaptchaValidator.Validate().IsValid;
         }
     }
 }
