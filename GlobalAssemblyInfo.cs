0a9e914beda17d065453d9b329851c8880eaffa7
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-04 14:56:01 +0200

    update copyright info

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("The NuGet Team")]
 [assembly: AssemblyProduct("PoliteCaptcha")]
-[assembly: AssemblyCopyright("Copyright (C) 2012 OuterCurve Foundation")]
+[assembly: AssemblyCopyright("Copyright (C) 2015 .NET Foundation")]
 [assembly: AssemblyVersion("0.4.0.0")]
 [assembly: ComVisible(false)]
